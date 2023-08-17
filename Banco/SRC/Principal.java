af68d1b8cf2b4036b09bdeb4fac8eaaaf915f31f
PabloMendes17 <firefoxpablo@gmail.com>
2023-08-17 15:21:09 -0300

    Commit_1521

--- Banco/SRC/Principal.java
+++ Banco/SRC/Principal.java
@@ -1,6 +1,23 @@
 public class Principal {
     public static void main(String[]args){
         Conta minhaConta = new Conta();
+        minhaConta.nomeTitular="Joao da Silva";
+        minhaConta.agencia=123;
+        minhaConta.numero=987;
+        minhaConta.saldo=15000;
+
+        Conta suaConta = new Conta();
+        suaConta.nomeTitular="Maria Abaidia";
+        suaConta.agencia=222;
+        suaConta.numero=333;
+        suaConta.saldo=30000;
+
+
+        System.out.println("Titular: "+minhaConta.nomeTitular);
+        System.out.println("Agencia: "+minhaConta.agencia);
+        System.out.println("Titular: "+suaConta.nomeTitular);
+        System.out.println("Agencia: "+suaConta.agencia);
+
 
     }
 }
