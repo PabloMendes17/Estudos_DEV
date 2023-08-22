f66882be2adb0b5b62c6c87db17c45bb079cae21
PabloMendes17 <firefoxpablo@gmail.com>
2023-08-22 15:44:56 -0300

    Tentaiva de Node_exclusão de pastas

---
+++ INTRO_JAVA/SRC/AlgoritimosDeOrdenacao.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class AlgoritimosDeOrdenacao{
+
+    // metodo sort
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6,7,7};
+        
+       
+    }
+
+
+
+}
