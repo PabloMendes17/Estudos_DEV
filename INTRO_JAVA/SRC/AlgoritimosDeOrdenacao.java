78484249cbc7054138428c50fdcca567729dc854
PabloMendes17 <firefoxpablo@gmail.com>
2023-08-25 06:06:01 -0300

    FimKanban inicio tests

--- INTRO_JAVA/SRC/AlgoritimosDeOrdenacao.java
+++ INTRO_JAVA/SRC/AlgoritimosDeOrdenacao.java
@@ -18,7 +18,32 @@ public class AlgoritimosDeOrdenacao{
             
         System.out.println("\nArray ordenado:");
         printArray(arBuble);
-        }
+        
+    
+    int[] arrSelection = {64, 34, 25, 12, 22, 11, 90};
+        
+                System.out.println("Array desordenado");
+                printArray(arrSelection);  
+        
+                selectionSort(arrSelection);
+        
+                System.out.println("\nArray ordenado:");
+                printArray(arrSelection);  
+                
+                
+
+                int[] arrQUICK = {64, 34, 25, 12, 22, 11, 90};
+
+                System.out.println("Array desordenado:");
+                printArrayQ(arrQUICK);
+        
+                quickSort(arrQUICK, 0, arrQUICK.length - 1);
+        
+                System.out.println("\nArray ordenado:");
+                printArrayQ(arrQUICK);            
+    
+    
+    }
     
         // Função para ordenar um array usando Bubble Sort
         static void bubbleSort(int[] arBuble) {
@@ -49,15 +74,92 @@ public class AlgoritimosDeOrdenacao{
         }
     
         // Função para imprimir um array
-        static void printArray(int[] arr) {
-            int n = arr.length;
+        static void printArray(int[] arrSelection) {
+            int n = arrSelection.length;
             for (int i = 0; i < n; i++) {
-                System.out.print(arr[i] + " ");
+                System.out.print(arrSelection[i] + " ");
             }
             System.out.println();
         }
+
+        
+           
+        
+            // Função para ordenar um array usando Selection Sort
+            static void selectionSort(int[] arrSelection) {
+                int n = arrSelection.length;
+        
+                for (int i = 0; i < n - 1; i++) {
+                    // Encontra o índice do menor elemento no restante do array
+                    int minIndex = i;
+                    for (int j = i + 1; j < n; j++) {
+                        if (arrSelection[j] < arrSelection[minIndex]) {
+                            minIndex = j;
+                        }
+                    }
+        
+                    // Troca o menor elemento encontrado com o elemento atual
+                    int temp = arrSelection[i];
+                    arrSelection[i] = arrSelection[minIndex];
+                    arrSelection[minIndex] = temp;
+                    for (int b:arrSelection){
+                        System.out.print(b+" ");
+                    }
+                }
+            }
+        
+    // Função para ordenar um subarray usando Quick Sort
+    static void quickSort(int[] arrQUICK, int low, int high) {
+        if (low < high) {
+            // Encontra o índice de partição, onde o array é dividido em duas partes
+            int partitionIndex = partition(arrQUICK, low, high);
+
+            // Ordena as duas partes separadamente
+            quickSort(arrQUICK, low, partitionIndex - 1);
+            quickSort(arrQUICK, partitionIndex + 1, high);
+        }
+    }
+
+    // Função para encontrar o índice de partição
+    static int partition(int[] arrQUICK, int low, int high) {
+        // Escolhe o elemento pivot (geralmente o último elemento)
+        int pivot = arrQUICK[high];
+
+        // Índice do menor elemento
+        int i = (low - 1);
+
+        for (int j = low; j <= high - 1; j++) {
+            // Se o elemento atual for menor ou igual ao pivot
+            if (arrQUICK[j] <= pivot) {
+                i++;
+
+                // Troca arr[i] e arr[j]
+                int temp = arrQUICK[i];
+                arrQUICK[i] = arrQUICK[j];
+                arrQUICK[j] = temp;
+            }
+        }
+
+        // Troca arr[i+1] e arr[high] (pivot)
+        int temp = arrQUICK[i + 1];
+        arrQUICK[i + 1] = arrQUICK[high];
+        arrQUICK[high] = temp;
+
+        return i + 1;
+    }
+
+    // Função para imprimir um array
+    static void printArrayQ(int[] arrQUICK) {
+        int n = arrQUICK.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arrQUICK[i] + " ");
+        }
+        System.out.println();
+    }
+}        
+        
+        
     
 
 
 
-}
